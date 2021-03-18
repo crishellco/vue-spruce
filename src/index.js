1db3b0f513e425aba13f4deb6afdb450367f4ed9
Chris Mitchell <cmitchell@crishell.co>
2021-03-18 01:10:07 -0400

    M coverage/lcov-report/Event.vue.html  M coverage/lcov-report/Function.vue.html  M coverage/lcov-report/Search.vue.html  M coverage/lcov-report/Sort.vue.html  M coverage/lcov-report/State.vue.html  M coverage/lcov-report/Toggle.vue.html  M coverage/lcov-report/index.html  M coverage/lcov-report/index.js.html  M package.json  M src/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import { RenderlessEvent, RenderlessState, RenderlessToggle } from './components';
+import {
+  RenderlessEvent,
+  RenderlessFunction,
+  RenderlessSearch,
+  RenderlessSort,
+  RenderlessState,
+  RenderlessToggle,
+} from './components';
 
-export { RenderlessEvent, RenderlessState, RenderlessToggle };
+export { RenderlessEvent, RenderlessFunction, RenderlessSearch, RenderlessSort, RenderlessState, RenderlessToggle };
 
 export default function install(Vue) {
   Vue.component('renderless-event', RenderlessEvent);
+  Vue.component('renderless-function', RenderlessFunction);
+  Vue.component('renderless-search', RenderlessSearch);
+  Vue.component('renderless-sort', RenderlessSort);
   Vue.component('renderless-state', RenderlessState);
   Vue.component('renderless-toggle', RenderlessToggle);
 }
