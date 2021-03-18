8cd3608831114ada3255bd01475a753a25432ebb
Chris Mitchell <cmitchell@crishell.co>
2021-03-17 21:39:52 -0400

    M coverage/lcov-report/Event.vue.html  M coverage/lcov-report/Search.vue.html  M coverage/lcov-report/Sort.vue.html  M coverage/lcov-report/State.vue.html  M coverage/lcov-report/Toggle.vue.html  M coverage/lcov-report/index.html  M coverage/lcov-report/index.js.html  M src/demo/components/Event.vue  M src/demo/components/Search.vue  M src/demo/components/Sort.vue  M src/demo/components/Toggle.vue

--- src/demo/components/Sort.vue
+++ src/demo/components/Sort.vue
@@ -92,7 +92,7 @@
         </renderless-state>
       </div>
     </div>
-    <div class="flex-1 w-1/2 overflow-hidden">
+    <div class="flex-1 w-1/2 overflow-hidden border-b border-gray-600">
       <pre class="whitespace-pre flex-1 flex flex-col w-full overflow-auto bg-gray-700 p-4">
         <code class="block text-xs font-mono text-white flex flex-1">{{ code }}</code>
       </pre>
